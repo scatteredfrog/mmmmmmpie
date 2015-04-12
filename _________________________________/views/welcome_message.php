7f9a6226e8d1ef1b2898f8b837074b0c51bb054f
Sean Courtney <seancourtney@fab4it.com>
2015-04-11 22:45:07 -0500

    Fake start

--- _________________________________/views/welcome_message.php
+++ _________________________________/views/welcome_message.php
@@ -1,5 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+if (substr($_SERVER['HTTP_HOST'],0,5) == 'local') {
+    error_log("IT'S LOCAL");
+}
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
