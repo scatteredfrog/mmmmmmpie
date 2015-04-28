fb2c82e3aa367799bcd3a14b062028f45c07e953
Sean Courtney <seancourtney@fab4it.com>
2015-04-28 06:08:49 -0500

    social media

--- _________________________________/views/main.php
+++ _________________________________/views/main.php
@@ -11,20 +11,37 @@
     <script src=<?=base_url("js/jquery-1.9.1.min.js"); ?>></script>
 </head>
 <body>
+    
+    <!-- Facebook crap -->
+    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+    <!-- end Facebook crap -->
+
     <div class="containerCenter">
         <div class="headerWrapper">
             <div id="header_image">
 
             </div>
             <div id="header_contact">
-                <div id="twit">
-                    <div id="twit0">Twitter: </div><div id="twit1">@PieFactory</div><div id="twit2">PFP</div><br />
+                <div id="soc_row">
+                    <div id="itunes">
+                        <a href="https://itunes.apple.com/us/podcast/pie-factory-podcast/id988509945?mt=2&uo=6&at=&ct=" target="itunes_store"></a>
+                    </div>
+                    <div id="twit">
+                        <div id="twit0"><a href="https://twitter.com/PieFactoryPFP" class="twitter-follow-button" data-show-count="false">Follow @PieFactoryPFP</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></div><br />
+                    </div>
+                    <div id="fbok">
+                        <div class="fb-like" data-href="https://www.facebook.com/PieFactoryPodcast" data-layout="button" data-action="like" data-show-faces="true"></div>
+                    </div>
                 </div>
                 <div id="eml">
-                    <div id="eml0">E-mail: </div><div id="eml1">pie</div><div id="eml2">factory@fab4it.co</div><div id="eml3">m</div><br />
-                </div>
-                <div id="fbk">
-                    <div id="fbk0">Facebook: </div><div id="fbk1"><a href="http://www.facebook.com/PieFactoryPodcast" target="_blank">http://www.facebook.com/PieFactoryPodcast</a></div>
+                        <div id="eml0">E-mail: </div><div id="eml1">pie</div><div id="eml2">factory@fab4it.co</div><div id="eml3">m</div><br />
                 </div>
             </div>
         </div>
