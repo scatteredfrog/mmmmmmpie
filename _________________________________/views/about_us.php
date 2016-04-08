49eb8c912e659ebf5c756d7c8ea0b42a8ba2424f
Sean Courtney <seancourtney@fab4it.com>
2016-04-08 07:45:03 -0500

    PFP-132 bio text added

--- _________________________________/views/about_us.php
+++ _________________________________/views/about_us.php
@@ -3,16 +3,9 @@
 
     </div>
     <div class="aboutYourHosts">
-        Jim and Sean, shown here discussing which games they'll <span class='strike'>look up on 
-            Wikipedia</span>
-        research diligently so they can have an intelligent discussion on the next episode, 
-        on a biweekly basis try to fulfill their mission to make Tinkle Pit a household name.<br />
-        &nbsp;<br />
-        In each episode, Sean and Jim discuss two (2) classic arcade games and reveal a common 
-        theme. Because they're longtime classic console gamers, the hosts also give a home gamer's 
-        perspective on the games they discuss. (And because both are from the Chicago area,
-        there's a bit of provinciality as well.)<br />
-        &nbsp;<br />
-        Oh...did we mention...Tinkle Pit?
+        <span ng-bind-html="aboutHosts"></span><br />&nbsp;<br />
+        <a ng-click="changeHost('hosts')">About The Hosts</a> &#183;
+        <a ng-click="changeHost('jim')">About Jimmy G</a> &#183; 
+        <a ng-click="changeHost('sean')">About Sean</a>
     </div>
 </div>
