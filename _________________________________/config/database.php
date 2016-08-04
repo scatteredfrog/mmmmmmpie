3d01315e5d1525ff4056266e65e1534d78eb3153
Sean Courtney <seancourtney@fab4it.com>
2016-08-04 13:41:02 -0500

    PFP-148 database adjustments for A2 Hosting

--- _________________________________/config/database.php
+++ _________________________________/config/database.php
@@ -95,7 +95,7 @@ if (substr($hs,0,5) === 'local' && stristr($rt,'Junk')) {
     $db['default']['database'] = 'fab4it_com';    
 } else {
     $db['default']['hostname'] = 'mysql.fab4it.com';
-    $db['default']['username'] = 'fab4itcom';
+    $db['default']['username'] = 'logthedo_fab4it';
     $db['default']['password'] = '4baf09282004db';
-    $db['default']['database'] = 'fab4it_com';
+    $db['default']['database'] = 'logthedo_fab4it_com';
 }
