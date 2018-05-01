5c40b8720cbdd53655bd5e131c088e77bc374f70
Sean Courtney <seancourtney@fab4it.com>
2018-04-30 20:15:53 -0500

    PFP-157 adding missing files

---
+++ _________________________________/views/mgc_interviews.php
@@ -0,0 +1,65 @@
+
+<table class="col-xs-6">
+    <tr>
+        <th width="25%" class="tableHeading">Interviewees</th>
+        <th width="50%" class="tableHeading">About</th>
+        <th width="25%" class="tableHeading">Download Links</th>
+    </tr>
+    <tr>
+        <td>
+            Bill from Atari Bytes and It's a Podcast, Charlie Brown
+        </td>
+        <td>
+            Jim and Sean chat and chill with Bill and discuss our thoughts and observations about Midwest 
+            Gaming Classic.
+        </td>
+        <td class="centerThis">
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/Bill_U.wav" target="_blank">WAV</a> | 
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/Bill_C.wav" target="_blank">WAV (censored)</a><br />
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/Bill_U.mp3" target="_blank">MP3</a> | 
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/Bill_C.mp3" target="_blank">MP3 (censored)</a>
+        </td>
+    </tr>
+    <tr class="lessBlack">
+        <td>
+            Tim Kitzrow (aka "Mr. Boomshakalaka")            
+        </td>
+        <td>
+            Tim shares his anecdotes about his career as a voice actor and talks a bit about the new 
+            version of Mutant Football League for PS4 and XBox One
+        </td>
+        <td class="centerThis">
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/TimKitzrow.wav" target="_blank">WAV</a> | 
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/TimKitzrow.mp3" target="_blank">MP3</a>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Tom Dunk from Guys Games and Beer
+        </td>
+        <td>
+            One of the key players from perhaps one of the most entertaining and informative gaming shows 
+            talks about the Saturday night festivities in the G2B room at Midwest Gaming Classic.
+        </td>
+        <td class="centerThis">
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/GGB_Tom_U.wav" target="_blank">WAV</a> | 
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/GGB_Tom_C.wav" target="_blank">WAV (censored)</a><br />
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/GGB_Tom_U.mp3" target="_blank">MP3</a> | 
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/GGB_Tom_C.mp3" target="_blank">MP3 (censored)</a>
+        </td>
+    </tr>
+    <tr class="lessBlack">
+        <td>
+            Walter Day and Billy Mitchell
+        </td>
+        <td>
+            Despite what had recently transpired before the show, this interview has nothing to do with 
+            anybody's questioned high scores, but rather the gaming community in general. Walter and Billy 
+            deliver some pretty big news for Pie Factory Podcast.
+        </td>
+        <td class="centerThis">
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/WalterBilly.wav" target="_blank">WAV</a> |
+            <a href="http://fab4it.com/piefactory/audio/mgc2018/WalterBilly.mp3" target="_blank">MP3</a>
+        </td>
+    </tr>
+</table>
